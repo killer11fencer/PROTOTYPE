27298b87a351a671fa86b5ef77102705392923f4
Jeoffrey <jeoff.howe@gmail.com>
2019-08-20 23:27:14 -0600

    toy problems

---
+++ Prototype/Test.jsx
@@ -0,0 +1,6 @@
+// write a function that combines two strings and whatever string is longer adds it to the end of the string.
+// every other letter
+
+// write a function that removes double letters and arranges how many changes to make a string saleable
+
+//
