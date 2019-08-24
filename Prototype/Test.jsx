339827e602651d21846b317631e4049fd2f2ffe6
Jeoffrey <jeoff.howe@gmail.com>
2019-08-23 20:49:46 -0600

    added

--- Prototype/Test.jsx
+++ Prototype/Test.jsx
@@ -1,6 +1,8 @@
 // write a function that combines two strings and whatever string is longer adds it to the end of the string.
 // every other letter
-
+function combine () {
+    
+}
 // write a function that removes double letters and arranges how many changes to make a string saleable
 
 //
