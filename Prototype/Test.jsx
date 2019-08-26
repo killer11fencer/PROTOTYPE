98481c1a2e69b42174b610282e49f5ab6391620f
Jeoffrey <jeoff.howe@gmail.com>
2019-08-26 11:23:22 -0600

    added finctions for toy problem

--- Prototype/Test.jsx
+++ Prototype/Test.jsx
@@ -1,10 +1,13 @@
 // write a function that combines two strings and whatever string is longer adds it to the end of the string.
 // every other letter
-function combine () {
-
+function combine (a,b) {
+let arr1 = a.split('')
+let arr2 = b.split('')
+console.log(arr1)
+ 
 }
 function double () {
-    
+
 }
 // write a function that removes double letters and arranges how many changes to make a string saleable
 
