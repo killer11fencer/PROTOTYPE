bd0d35b7cae2ed48678f0a6f58330ea7f1d6e22d
Jeoffrey <jeoff.howe@gmail.com>
2019-08-27 11:21:17 -0600

    solved function of adding two strings every other toy problem

--- Prototype/Test.jsx
+++ Prototype/Test.jsx
@@ -1,11 +1,23 @@
 // write a function that combines two strings and whatever string is longer adds it to the end of the string.
 // every other letter
 function combine (a,b) {
-let arr1 = a.split('')
-let arr2 = b.split('')
-console.log(arr1)
- 
-}
+    let arr1 = a.split('')
+    let arr2 = b.split('')
+    let arr3 = arr1.map((letter,i)=>{
+        if(i < arr2.length) {
+        return arr1[i] + arr2[i]}
+    })
+    if(arr2.length > arr1.length) {
+      let arr4 = arr2.slice(arr1.length,arr2.length)
+      return arr3.concat(arr4).join('')
+     } else if (arr1.length > arr2.length) {
+       let arr4 = arr1.slice(arr2.length,arr1.length)
+       return arr3.concat(arr4).join('')
+     }
+     return arr3.join('')
+    
+    }
+     
 function double () {
 
 }
